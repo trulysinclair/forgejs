d79895e721ede593b3764af1a5ef8ce024b6e939
Sinclair <truly.sinclair@gmail.com>
2023-06-17 21:01:53 -0400

    add inversify

---
+++ libs/control/src/lib/createDecorator.ts
@@ -0,0 +1,25 @@
+import { inject } from "inversify";
+
+/**
+ * A wrapper around Inversify's `@inject` decorator. Used to gracefully inject a dependency into a class.
+ *
+ * @param identifier The identifier of the dependency to inject, can be a string or a symbol.
+ *
+ *
+ * ```ts
+ * import { createDecorator } from '@forgejs/control';
+ * import { LogService } from './log-service';
+ *
+ * const ILogService = createDecorator<ILogService>("LogService");
+ *
+ * class MyController {
+ *  constructor(@IService private readonly service: MyService) {}
+ * }
+ *
+ * ```
+ */
+export function createDecorator<T>(
+  identifier: string | symbol,
+) {
+  return inject<T>(identifier);
+}
